1eba11946903619be8cc2d8bd2cdcf377c3fd06b
Evgenij <murzin90@yandex.ru>
2022-09-06 23:39:59 +0700

    commit

---
+++ src/js/modules/slider/slider-main.js
@@ -0,0 +1,79 @@
+import Slider  from './slider';
+
+export default class MainSlider extends Slider {
+    constructor(btns) {
+        super(btns);
+    }
+
+    showSlides(n) {
+        if (n > this.slides.length) {
+            this.slideIndex = 1;
+        }
+
+        if (n < 1) {
+            this.slideIndex = this.slides.length;
+        }
+
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch(error) {}
+
+        this.slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+
+        this.slides[this.slideIndex - 1].style.display = 'block';
+    }
+
+    plusSlides(n) {
+        this.showSlides(this.slideIndex += n);
+    }
+
+    btnSelect( module,n) {
+        module.forEach(item => {
+            item.addEventListener('click', (event) => {
+                event.stopPropagation();
+                event.preventDefault();
+                this.plusSlides(n);
+            });
+        });
+    }
+
+    bindTriggers() {
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                this.plusSlides(1);
+            });
+
+            btn.parentNode.previousElementSibling.addEventListener('click', (event) => {
+                event.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            });
+        });
+
+        this.btnSelect(this.prevModule, -1);
+        this.btnSelect(this.nextModule, 1);
+    }
+
+    render() {
+        if (this.container) {
+            try {
+                this.hanson = document.querySelector('.hanson');
+            } catch(error) {}
+
+            this.showSlides(this.slideIndex);
+            this.bindTriggers();
+        }
+    }
+}
