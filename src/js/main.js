e1f43ef5330b322c1df8076903330e69aad7cb42
Evgenij <murzin90@yandex.ru>
2022-10-22 19:03:10 +0700

    added link color change on mouse hover

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,7 @@
-import MainSlider from './modules/slider/slider-main';
-import MiniSlider from './modules/slider/slider-mini';
-import VideoPlayer from './modules/playVideo';
-import Difference from './modules/difference';
-import Form from './modules/forms';
-import ShowInfo from './modules/showinfo';
-import DownLoad from './modules/download';
+import ChangeLinkColor from "./modules/changelinkcolor";
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({btns: '.next', container: '.page'});
-    slider.render();
+    const linkColor = new ChangeLinkColor('.header__menu-item', '.category__block');
 
-    const modulePageSlider = new MainSlider({
-        container: '.moduleapp',
-        btns: '.next',
-        prevModule: '.prevmodule',
-        nextModule: '.nextmodule'
-    });
-    modulePageSlider.render();
-
-    const showUpSlider = new MiniSlider({
-        container: '.showup__content-slider',
-        prev: '.showup__prev',
-        next: '.showup__next',
-        activeClass: 'card-active',
-        animate: true
-    });
-
-    showUpSlider.init();
-
-    const modulesSlider = new MiniSlider({
-        container: '.modules__content-slider',
-        prev: '.modules__info-btns .slick-prev',
-        next: '.modules__info-btns .slick-next',
-        activeClass: 'card-active',
-        animate: true,
-        autoplay: true
-    });
-
-    modulesSlider.init();
-
-    const feedSlider = new MiniSlider({
-        container: '.feed__slider',
-        prev: '.feed__slider .slick-prev',
-        next: '.feed__slider .slick-next',
-        activeClass: 'feed__item-active'
-    });
-
-    feedSlider.init();
-
-    new VideoPlayer('.showup .play', '.overlay').init();
-    new VideoPlayer('.module__video-item .play', '.overlay').init();
-    new Difference('.officerold', '.officernew', '.officer__card-item').init();
-    new Form('.form').init();
-    new ShowInfo('.plus__content').init();
-    new DownLoad('.download').init();
+    linkColor.init();
 });
