29ff53f8eabf9f0d607ce86e4813707f35f4df8c
Evgenij <murzin90@yandex.ru>
2022-10-23 21:14:30 +0700

    added link color change on mouse hover

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import ChangeLinkColor from "./modules/changelinkcolor";
 
 window.addEventListener('DOMContentLoaded', () => {
     const linkColor = new ChangeLinkColor('.header__menu-item');
-    
+
     linkColor.init();
+    
 });
